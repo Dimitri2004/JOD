9f1ca315018c80016afef5f9bb4dbb9982952cdd
Dima <dapariciopereira@danielcastelao.org>
2025-04-03 13:38:58 +0200

    Principio 0.3

--- Juego1/juego1.js
+++ Juego1/juego1.js
@@ -1,26 +1,64 @@
 const objeto = document.getElementById("miObjeto");
 const contenedor = document.getElementById("contenedor");
-        let posicionx = 10; // Posición inicial en el eje X
-        let velocidadx = 3; // Velocidad en el eje X
+        let posicionx1 = 883; // Posición inicial en el eje y
+        let velocidadx1 = 5; // Velocidad en el eje X
 
         function moverObjeto() {
-            const anchoContenedor = contenedor.offsetWidth; // Ancho de la ventana
-            const anchoObjeto = objeto.offsetHeight;
+            const altoContenedor = contenedor.offsetHeight; // Ancho de la ventana
+            const altoObjeto = objeto.offsetHeight;
 
             // Actualizamos la posición en el eje X
-            posicionx += velocidadx;
+           
 
             // Detectamos colisiones con los bordes horizontales
-            if (posicionx <= 10 || posicionx + anchoObjeto > anchoContenedor -10) {
-                velocidadx *= -1; // Invertimos la dirección en el eje X
+            if (posicionx1 < distancia1) {
+                posicionx1 += velocidadx1;
+                velocidadx1 *= +1; // Invertimos la dirección en el eje X
+                // Aplicamos la nueva posición al estilo del objeto
+            objeto.style.left= posicionx1 + "px";
+            }else {
+                clearInterval(intervalo); // Detenemos el movimiento
+                console.log("El objeto alcanzó la distancia objetivo.");
             }
             
-            // Aplicamos la nueva posición al estilo del objeto
-            objeto.style.left = posicionx + "px";
+            
         }
 
         // Llamamos a la función `moverObjeto` repetidamente
-        setInterval(moverObjeto, 10);
+        setInterval(moverObjeto, 20);
+
+
+
+const objetivo = document.getElementById("miObjetivo");
+const contenedor2 = document.getElementById("contenedor");
+const distancia1 =1400;
+const distancia2 =395;
+        let posicionx2 = 920; // Posición inicial en el eje y
+        let velocidadx2 = 5; // Velocidad en el eje X
+
+        function moverObjetivo() {
+            const altoContenedor = contenedor2.offsetHeight; // Ancho de la ventana
+            const altoObjetivo = objetivo.offsetHeight;
+
+            
+
+            // Detectamos colisiones con los bordes horizontales
+            if (posicionx2 > distancia2) {
+                // Actualizamos la posición en el eje X
+            posicionx2 -= velocidadx2;
+                velocidadx2 *= +1; // Invertimos la dirección en el eje X
+                // Aplicamos la nueva posición al estilo del objeto
+            objetivo.style.left= posicionx2 + "px";
+            } else {
+                clearInterval(intervalo); // Detenemos el movimiento
+                console.log("El objeto alcanzó la distancia objetivo.");
+            }
+            
+            
+        }
+
+        // Llamamos a la función `moverObjeto` repetidamente
+        setInterval(moverObjetivo, 20);
 
 
 
