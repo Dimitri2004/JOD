be6a6f443c0987b6d9a1ffd41d2e044a32a33e8c
Dima <dapariciopereira@danielcastelao.org>
2025-04-19 12:38:46 +0200

    Juego update 0.5

--- Juego1/juego1.js
+++ Juego1/juego1.js
@@ -1,18 +1,11 @@
-const objeto = document.getElementById("miObjeto");
-const contenedor = document.getElementById("contenedor");
-let posicionx1 = 850; // Posición inicial en el eje y
+const objeto = document.getElementById("Player1");
+let posicionx1 = 450; // Posición inicial en el eje y
 let velocidadx1 = 0.5; // Velocidad en el eje X
-
+let distancia1=150;
 function moverObjeto() {
-    const altoContenedor = contenedor.offsetHeight; // Ancho de la ventana
-    const altoObjeto = objeto.offsetHeight;
-
-    // Actualizamos la posición en el eje X
-
-
     // Detectamos colisiones con los bordes horizontales
-    if (posicionx1 < distancia1) {
-        posicionx1 += velocidadx1;
+    if (posicionx1>distancia1) {
+        posicionx1 -= velocidadx1;
         velocidadx1 *= +1; // Invertimos la dirección en el eje X
         // Aplicamos la nueva posición al estilo del objeto
         objeto.style.left = posicionx1 + "px";
@@ -21,33 +14,6 @@ function moverObjeto() {
 
 // Llamamos a la función `moverObjeto` repetidamente
 setInterval(moverObjeto, 20);
-
-const objetivo = document.getElementById("miObjetivo");
-const contenedor2 = document.getElementById("contenedor");
-
-
-const distancia1 = 1200;
-const distancia2 = 440;
-let posicionx2 = 790; // Posición inicial en el eje y
-let velocidadx2 = 0.5; // Velocidad en el eje X
-
-function moverObjetivo() {
-    const altoContenedor = contenedor2.offsetHeight; // Ancho de la ventana
-    const altoObjetivo = objetivo.offsetHeight;
-
-    // Detectamos colisiones con los bordes horizontales
-    if (posicionx2 > distancia2) {
-        // Actualizamos la posición en el eje X
-        posicionx2 -= velocidadx2;
-        velocidadx2 *= +1; // Invertimos la dirección en el eje X
-        // Aplicamos la nueva posición al estilo del objeto
-        objetivo.style.left = posicionx2 + "px";
-    }
-
-}
-
-
-
 
 // localizamos el objeto que usaremos para mostrar el tiempo
 const temporizador = document.querySelector("#tiempo")
@@ -66,7 +32,6 @@ const temporizador = document.querySelector("#tiempo")
             temporizador.innerHTML = "YA"
         }
     }
-
     // Funcion para encender el temporizador con 3 segundos
     function encenderTemporizador(){
         tiempoRestante = 3;
